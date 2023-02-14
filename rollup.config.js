3bc08ecd7e55f722b5feaa9cc9c556241b9a6947
cola639 <337507950@qq.com>
2023-02-14 16:56:47 +0800

    commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from '@rollup/plugin-babel'
 import svg from 'rollup-plugin-svg'
-import styles from "rollup-plugin-styles"
+import styles from 'rollup-plugin-styles'
 const autoprefixer = require('autoprefixer')
 import typescript from '@rollup/plugin-typescript'
 
@@ -9,43 +9,32 @@ const input = 'src/index.ts'
 let config = []
 let MODE = [{ fomart: 'esm' }, { fomart: 'umd' }]
 
-
 MODE.map((m) => {
   let indexTs = {
     input: input,
     output: {
       // then name of your package
-      name: "react-material-fileuploader",
+      name: 'mui-cola638',
       file: `dist/index.${m.fomart}.js`,
       format: m.fomart,
-      exports: "auto",
+      exports: 'auto',
       sourcemap: true,
       globals: {
-        'react': 'React',
+        react: 'React',
         'prop-types': 'PropTypes',
         '@mui/material': '@mui/material',
         '@babel/runtime': '@babel/runtime',
-        '@mui/icons-material': '@mui/icons-material'
+        '@mui/icons-material': '@mui/icons-material',
       },
     },
     // this externelizes react to prevent rollup from compiling it
-    external: [
-      "react",
-      "@mui/icons-material",
-      "@mui/lab",
-      "@mui/material",
-      "prop-types",
-      /@babel\/runtime/
-    ],
+    external: ['react', '@mui/icons-material', '@mui/lab', '@mui/material', 'prop-types', /@babel\/runtime/],
     plugins: [
       // these are babel comfigurations
       babel({
         exclude: 'node_modules/**',
-        plugins: [
-          '@babel/transform-runtime', 
-          "@babel/plugin-proposal-optional-chaining"
-        ],
-        babelHelpers: 'runtime'
+        plugins: ['@babel/transform-runtime', '@babel/plugin-proposal-optional-chaining'],
+        babelHelpers: 'runtime',
       }),
       typescript({
         tsconfig: './tsconfig.json',
@@ -54,15 +43,13 @@ MODE.map((m) => {
       // this adds support for styles
       styles({
         postcss: {
-          plugins: [
-            autoprefixer()
-          ]
-        }
-      })
-    ]
+          plugins: [autoprefixer()],
+        },
+      }),
+    ],
   }
 
   config.push(indexTs)
 })
 
-export default [ ...config ]
+export default [...config]
